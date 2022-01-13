a95dc35d2d5265bc1ce87fc9b4cfbdfde0d145c6
SIVA KUMAR <78172724+siva25041998@users.noreply.github.com>
2022-01-13 09:21:49 +0530

    Update Example.js

--- Example.js
+++ Example.js
@@ -1,16 +1,19 @@
 import React from "react";
-import CustomSelector from "./CustomSelector.js";
+import CustomSelector from "CustomSelector.js";
 
 
 function Example{
 	
-	const Options = [1 ,2 , '3', 'Four', 'Five']
+	const weekDays = [1 ,2 , 3, 4, 5, 6 ,7];
+	const Months = ['jan' ,'Feb' , 'MAr', 'Apr', 'May', 'jun', 'jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 	return(
 		<>
-			<CustomSelector options={Options} />
+			<CustomSelector options={weekDays} />
+			<CustomSelector options={Months} />
 		</>
 
 	)
 }
 
 export default Example
+//Thank You
