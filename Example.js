43df6b46eea9f858ed882fbf6cba5f1ff1ec4f5c
SIVA KUMAR <78172724+siva25041998@users.noreply.github.com>
2022-01-13 09:48:45 +0530

    Update Example.js

--- Example.js
+++ Example.js
@@ -5,7 +5,7 @@ import CustomSelector from "CustomSelector.js";
 function Example{
 	
 	const weekDays = [1 ,2 , 3, 4, 5, 6 ,7];
-	const Months = ['jan' ,'Feb' , 'MAr', 'Apr', 'May', 'jun', 'jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+	const Months = ['jan' ,'Feb' , 'Mar', 'Apr', 'May', 'jun', 'jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 	return(
 		<>
 			<CustomSelector options={weekDays} className= 'use classes as per your need' />
