5ca8bc79928de15a7ee85d1c991f67ad8b1e0cbb
Siva kumar <mptk1208@gmail.com>
2022-12-10 21:19:30 +0530

    error Fixed

--- Example.js
+++ Example.js
@@ -2,13 +2,13 @@ import React from "react";
 import CustomSelector from "CustomSelector.js";
 
 
-function Example{
+function Example(){
 	
 	const weekDays = [1 ,2 , 3, 4, 5, 6 ,7];
 	const Months = ['jan' ,'Feb' , 'Mar', 'Apr', 'May', 'jun', 'jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 	return(
 		<>
-			<CustomSelector options={weekDays} className= 'use classes as per your need' />
+			<CustomSelector options={weekDays} className= 'className' />
 			<CustomSelector options={Months} />
 		</>
 
